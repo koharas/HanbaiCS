f3530759c9d2caa288e2fda4182ea98c50592f24
dicreltd <skohara@dicre.com>
2020-06-10 21:01:34 +0900

    driver update

--- HanbaiCS/ShouhinDAO.cs
+++ HanbaiCS/ShouhinDAO.cs
@@ -1,10 +1,5 @@
-﻿using MySql.Data.MySqlClient;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data;
+using MySql.Data.MySqlClient;
 
 namespace HanbaiCS
 {
